e2cec59ce9a35eaec3a450f4aa07d8722ee125be
2sammy <samuelmanoah61@gmail.com>
2023-08-13 10:01:30 +0300

    events

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,15 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log('hello sam')
+    }
+    const handleClickAgain = (name, e) => {
+        console.log('hello' + name, e.target);
+    }
     return ( 
         <div className ="home">
             <h2> Homepage</h2>
+            <button onClick = {handleClick}>Click Me</button>
+            <button onClick= {(e) => handleClickAgain('sanchez') }>Click</button>
         </div>
      );
 }
