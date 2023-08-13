946ab9abb28265526556aefdce56021be9977e45
2sammy <samuelmanoah61@gmail.com>
2023-08-13 08:52:35 +0300

    dealing with components

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+  const title = "Welcome to my Awewsome blog"
+  const likes = 50
   return (
     <div className="App">
-      
+      <div className='content'>
+        <h1>{title}</h1>
+        <p>Loved with {likes} times</p>
+        <p>{"iam able to this"}</p>
+        <p>{100} likes</p>
+        <p>{["sammy", "rissh"]}</p>
+
+      </div>
     </div>
   );
 }
