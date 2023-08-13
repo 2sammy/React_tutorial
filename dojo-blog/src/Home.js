ef03102152447492bb468ba421a0993a30217e6e
2sammy <samuelmanoah61@gmail.com>
2023-08-13 10:21:21 +0300

    hooks

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,17 +1,25 @@
+import {useState} from "react";
+
 const Home = () => {
-    const handleClick = (e) => {
-        console.log('hello sam')
+
+    const [name, setName] = useState('sam');
+    const [age, setAge] = useState(24);
+
+    const handleClick = () => {
+        setName ('manaoh');
+       
+        
     }
-    const handleClickAgain = (name, e) => {
-        console.log('hello' + name, e.target);
-    }
+    
     return ( 
         <div className ="home">
             <h2> Homepage</h2>
+            <p> {name} is { age} years old</p>
             <button onClick = {handleClick}>Click Me</button>
-            <button onClick= {(e) => handleClickAgain('sanchez') }>Click</button>
+        
         </div>
      );
-}
+    }
+
  
 export default Home;
