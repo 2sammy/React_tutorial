2145c59e3744f3a21306a6bf1b13e3a5204afedf
2sammy <samuelmanoah61@gmail.com>
2023-09-03 11:48:00 +0300

    components, adding styles,adding buttons

---
+++ dojo-blog/src/practiceApp.js
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+import Home from "./Home";
+
+ function App () {
+    <div className="App">
+        <Navbar />
+        <div className="content">
+            <Home />
+        </div>
+
+    </div>
+
+
+ }
---
+++ dojo-blog/src/practiceHome.js
@@ -0,0 +1,15 @@
+const Home = () => {
+    const handleClick = () => {
+        console.log('hello Sam')
+    }
+    return ( 
+        <div className="home">
+            <h2>Home</h2>
+            <button>Click Me</button>
+
+        </div>
+
+     );
+}
+ 
+export default Home;
