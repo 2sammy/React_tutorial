987c29da1a9eb4730bca0736915495631f3c9040
2sammy <samuelmanoah61@gmail.com>
2023-09-03 17:55:01 +0300

    post method

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -1,8 +1,68 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+    const [author, setAuthor] = useState('yoshi')
+    const [isPending,setIsPending] = useState(false)
+
+    const handlSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }) 
+        .then(() =>{
+            console.log('new block added')
+            setIsPending(false);
+        })
+
+
+    }
+
+
 
     return ( 
         <div className="create">
             <h2>Add A new Blog</h2>
+            <form onSubmit={handlSubmit}>
+                <label>
+                    Blog title:</label>
+                <input 
+                type="text"
+                required
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                
+                />
+                <label> Blog body: </label>
+                <textarea
+                 required 
+                 value={body}
+                 onChange={(e) => setBody(e.target.value)}
+
+                 
+                 > </textarea>
+                <label>Blog Author</label>
+                <select
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+                
+                >
+            
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                
+                    </select>
+                    {!isPending && <button> Add Blog</button>}
+                    {isPending && <button disabled>  Adding blog...</button>}
+                    <p>{title}</p>
+                    <p>{body}</p>
+                    <p>{author}</p>
+            </form>
 
         </div>
 
